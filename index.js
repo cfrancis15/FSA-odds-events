f4dc6161c1f32bc247df03ecc30ee4c09d9369e7
Connor Francis <connorfrancis@live.com>
2026-01-12 00:32:29 -0700

    built part of odds function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let numberBank = []
 
 function addToBank(n){
-    numberBank.push(n+'')
+    numberBank.push(n)
     console.log(numberBank)
     return numberBank
     
@@ -20,9 +20,11 @@ function NumberForm() {
     <button>Sort All</button>
     <h2>Bank</h2>
     <section class='section'></section>
+
+
   `;
     const $section = $form.querySelector(".section")
-    
+
 
 
   $form.addEventListener("submit",function (event){
@@ -40,7 +42,18 @@ function NumberForm() {
 /////////////////////////////////////Good above this line
 
 
+function Sort1(){
+    const $sort1 = document.createElement('sort1')
+    $sort1.innerHTML = `
+        <h2>Odds</h2>
+        <section class='section'></section>
+    `
 
+    return $sort1
+
+    //add event listener to sort based on button press in other function
+
+}
 
 
 
@@ -54,13 +67,14 @@ function render(){
 
     <h1>Odds and Events</h1>
     <NumberForm></NumberForm>
-
+    <Sort1></Sort1>
 
 
     </main>
   `;
 
     $app.querySelector("NumberForm").replaceWith(NumberForm());
+    $app.querySelector("Sort1").replaceWith(Sort1());
 
 
 
